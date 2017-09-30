8657e70080381a3fcad8a34e279ea8771f446082
DomeA <domeastudio@gmail.com>
2017-09-30 17:52:10 +0800

    项目 init

---
+++ code/Leo/mappingo/servers/micro-service/h2/src/main/java/com/domeastudio/mappingo/servers/microservice/h2/pojo/FieldTypeEnum.java
@@ -0,0 +1,15 @@
+package com.domeastudio.mappingo.servers.microservice.h2.pojo;
+
+public enum FieldTypeEnum {
+    NULL,
+    Char,
+    String,
+    Byte,
+    Int,
+    Long,
+    Double,
+    Float,
+    Date,
+    File,
+    UUID
+}
