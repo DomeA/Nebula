313a77428a80d5671981d149410a77e2644937cc
DomeA <domeastudio@gmail.com>
2017-10-06 21:22:22 +0800

    定义方法

--- code/Leo/mappingo/servers/micro-service/gis-process/src/main/java/com/domeastudio/mappingo/server/microservice/gisprocess/core/ReadShp2DB.java
+++ code/Leo/mappingo/servers/micro-service/gis-process/src/main/java/com/domeastudio/mappingo/server/microservice/gisprocess/core/ReadShp2DB.java
@@ -1,4 +1,6 @@
 package com.domeastudio.mappingo.server.microservice.gisprocess.core;
 
 public class ReadShp2DB {
+    public void readShp(){}
+    public void writeShp2DB(){}
 }
--- code/Leo/mappingo/servers/micro-service/gis-process/src/main/java/com/domeastudio/mappingo/server/microservice/gisprocess/core/WriteShp4DB.java
+++ code/Leo/mappingo/servers/micro-service/gis-process/src/main/java/com/domeastudio/mappingo/server/microservice/gisprocess/core/WriteShp4DB.java
@@ -1,4 +1,6 @@
 package com.domeastudio.mappingo.server.microservice.gisprocess.core;
 
 public class WriteShp4DB {
+    public void readShp4DB(){}
+    public void writeShp(){}
 }
